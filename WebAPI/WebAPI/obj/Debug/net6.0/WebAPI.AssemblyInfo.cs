48c6687ef91f5b9e251d1718284f1d94f7a21db3
hcdung <dungb1906640@gmail.com>
2024-03-11 08:39:32 +0700

    revert datatable

--- WebAPI/WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e243ca9c9be6ad6535bcd85ca6a03e00ced1c578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d8455097c002737d69b1ec1b7fec63241708d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
