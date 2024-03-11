8e04a0322dc5409b3f39941c0c05ac4ae07c8153
dung-huynh001 <dungb1906640@gmail.com>
2024-03-11 08:38:18 +0700

    Add Datatable

--- ClientApp/src/app/pages/department-management/department-management.component.ts
+++ ClientApp/src/app/pages/department-management/department-management.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-department-management',
@@ -7,13 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentManagementComponent implements OnInit {
   breadCrumbItems!: Array<{}>;
+  maxFileSize: number = 2097152;
+  formAddDepartment!: FormGroup;
+  submitted: boolean = false;
 
-  constructor() {}
+  constructor(private _modalService: NgbModal, private formBuilder: FormBuilder) { }
+
+  get formAdd() {
+    return this.formAddDepartment.controls;
+  }
 
   ngOnInit(): void {
     this.breadCrumbItems = [
       { label: 'Home' },
-      { label: 'Department Management', active: true}
-    ]
+      { label: 'Department Management', active: true }
+    ];
+
+    this.formAddDepartment = this.formBuilder.group({
+      departmentName: ['', Validators.required],
+      image: ['', [Validators.required, Validators.max(this.maxFileSize)]]
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.formAddDepartment.valid) {
+      this.resetForm();
+    }
+  }
+
+  resetForm() {
+    this.formAddDepartment.reset();
+    this.submitted = false;
+    this._modalService.dismissAll();
+  }
+
+  onFileSelected(event: any) {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      if (selectedFile.size > this.maxFileSize) {
+        this.formAdd['image'].setErrors({ max: true });
+        return;
+      }
+    }
+  }
+
+  onClose() {
+    document.getElementsByClassName('modal')
+  }
+
+  openModal(content: any) {
+    this._modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg', centered: true }).result.then(
+      () => { },
+      () => { this.resetForm(); });
   }
 }
